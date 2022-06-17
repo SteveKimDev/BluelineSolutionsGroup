273731e8100c329b01821b09c4e2541a56b8e67b
stevekim72 <steve.kim72@yahoo.com>
2022-06-17 16:10:21 -0700

    intial set up

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,52 @@
 module.exports = {
   siteMetadata: {
-    title: `blueline-solutions-group`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Blueline Solutions Group`,
+    description: `Serving logistical services to domestic and international markets.`,
+    author: `@stevekimdev`,
+    siteUrl: `https://bluelinesolutionsgroup.com`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: `${__dirname}/src/images/`,
+      },
     },
-    __key: "images"
-  }]
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: `Blueline Solutions Group`,
+        short_name: `Blueline SG`,
+        start_url: `/`,
+        background_color: `#222222`,
+        theme_color: `#222222`,
+        display: `standalone`,
+        cache_busting_mode: `none`,
+        icon: "src/images/icon.png",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /svg/,
+          omitKeys: [
+            "xmlnsDc",
+            "xmlnsCc",
+            "xmlnsRdf",
+            "xmlnsSvg",
+            "xmlnsSodipodi",
+            "xmlnsInkscape",
+            "xmlnsSerif",
+            "serifId",
+          ],
+        },
+      },
+    },
+  ],
 };
---
+++ src/components/layout/layout.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { SSRProvider } from 'react-bootstrap';
+
+// import Header from './header';
+// import Footer from './footer';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import './global-style/layout.css';
+
+const Layout = ({ children }) => {
+  return (
+    <SSRProvider>
+      <div className={`screen-orientation`}>
+        {/* <Header /> */}
+        <main className={`main-content`}>{children}</main>
+        {/* <Footer /> */}
+      </div>
+    </SSRProvider>
+  );
+};
+
+export default Layout;
