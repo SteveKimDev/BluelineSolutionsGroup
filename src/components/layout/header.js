2017d13f5e7b3bcf5372352a52b05a77b3846581
stevekim72 <steve.kim72@yahoo.com>
2022-06-22 13:24:39 -0700

    animation header

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,16 +1,23 @@
 import * as React from 'react';
+import { useSpring, animated } from 'react-spring';
 
 import Logo from './logo';
 
 import * as HeaderStyle from './global-style/header.module.css';
 
 const Header = () => {
+  const fadeIn = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    delay: 1000,
+    config: { duration: 500 },
+  });
   return (
     <div className={HeaderStyle.headerDiv}>
       <Logo />
-      <span className={HeaderStyle.linkDiv}>
+      <animated.span style={fadeIn} className={HeaderStyle.linkDiv}>
         <a href='mailto:info@bluelinesolutionsgroup.com'>Contact Us</a>
-      </span>
+      </animated.span>
     </div>
   );
 };
--- src/components/layout/logo.js
+++ src/components/layout/logo.js
@@ -1,17 +1,24 @@
 import * as React from 'react';
+import { useSpring, animated } from 'react-spring';
 import { StaticImage } from 'gatsby-plugin-image';
 
 import * as HeaderStyle from './global-style/header.module.css';
 
 const Logo = () => {
+  const fadeIn = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    delay: 500,
+    config: { duration: 500 },
+  });
   return (
-    <div>
+    <animated.div style={fadeIn}>
       <StaticImage
         src='https://res.cloudinary.com/drqcptpet/image/upload/q_auto/v1655850943/Blueline%20Solutions%20Group/blueline-solutions-logo-no-text_zeeowh.webp'
         alt='logo img'
         className={`${HeaderStyle.logo}`}
       />
-    </div>
+    </animated.div>
   );
 };
 
